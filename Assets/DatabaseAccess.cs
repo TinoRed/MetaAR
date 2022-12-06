c4bc5611b435487f36142ff286458faa5e7dd703
Agostino Reddel <agostino.reddel@gmail.com>
2022-12-06 16:33:46 +0100

    feat(DatabaseAccessClass): added class for testing mongoDB dlls

---
+++ Assets/DatabaseAccess.cs
@@ -0,0 +1,31 @@
+using MongoDB.Driver;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MongoDB.Bson;
+
+public class DatabaseAccess : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private const string MONGO_URI = "mongodb+srv://agost:agost@cluster0.vhjvg.mongodb.net/?retryWrites=true&w=majority";
+    private const string DATABASE_NAME = "test";
+    private MongoClient client;
+    private IMongoDatabase db;
+    private IMongoCollection<BsonDocument> collection;
+
+    void Start()
+    {
+        client = new MongoClient(MONGO_URI);
+        db = client.GetDatabase(DATABASE_NAME);
+        collection = db.GetCollection<BsonDocument>("test");
+        //Test
+        var document = new BsonDocument {{"username",100}};
+        collection.InsertOne(document);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
